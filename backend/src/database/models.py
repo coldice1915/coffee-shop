a2ff6efc94e73b137f799003fd805a9b51e021e8
coldice1915 <coldice.zs44@gmail.com>
2020-02-26 18:10:08 -0800

    add test data

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -14,26 +14,56 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
+
 '''
 db_drop_and_create_all()
     drops the database tables and starts fresh
     can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    !!NOTE you can change the database_filename variable to
+    have multiple verisons of a database
 '''
+
+
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+    db_test_data()
+
+
+def db_test_data():
+    Americano = (Drink(
+        id=1,
+        title='Americano',
+        recipe="""[
+                {
+                    "name" : "hotwater",
+                    "color": "skyblue",
+                    "parts": 3
+                },
+                {
+                    "name" : "espresso",
+                    "color": "brown",
+                    "parts": 1
+                }
+        ]"""
+        ))
+    Americano.insert()
+
 
 '''
 Drink
 a persistent drink entity, extends the base SQLAlchemy Model
 '''
+
+
 class Drink(db.Model):
     # Autoincrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
@@ -42,7 +72,7 @@ class Drink(db.Model):
     # the ingredients blob - this stores a lazy json blob
     # the required datatype is [{'color': string, 'name':string,
     #                           'parts':number}]
-    recipe =  Column(String(180), nullable=False)
+    recipe = Column(String(180), nullable=False)
 
     '''
     short()
@@ -50,7 +80,8 @@ class Drink(db.Model):
     '''
     def short(self):
         print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        short_recipe = [{'color': r['color'], 'parts': r[
+            'parts']} for r in json.loads(self.recipe)]
         return {
             'id': self.id,
             'title': self.title,
